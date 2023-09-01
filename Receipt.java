09d8db59ef8878b70b37939d442a1e0e51dfee6f
TheEpikDuck <zhujul24@7hills.org>
2023-09-01 12:45:09 -0400

    stuff

---
+++ Receipt.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Receipt
+{
+    public static void main (String[] args)
+    {
+        int priceWaffle = 25;
+        int priceNuggets + 
+        int pricePotatoes
+        String descWaffle
+        String descNugget
+        String descPotatoes
+        
+        System.out.print("Welcome to Julia's store buy something or else u die :D ");
+        System.out.println("We sell waffles, chicken nuggets and potatoes would you like to buy anything? ");
+        {
+            if yes;
+            Scanner scan = new Scanner(System.in)
+            System.out.println(" Our deliciously ")
+            
+        }
+
+
+
+    }
+}
